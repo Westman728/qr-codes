6c7362793472cb25408e3341feeef8fab51352a4
Westman728 <r.westman93@gmail.com>
2025-06-02 09:18:39 +0200

    Added functions/layouts for all pages

--- QRTools/wwwroot/js/qr-generators.js
+++ QRTools/wwwroot/js/qr-generators.js
@@ -1,31 +1,106 @@
+function generateQrCode() {
+    const inputText = document.getElementById("qr-input").value;
+
+    if (!inputText) {
+        !alert("Please enter a URL!");
+        return;
+    }
+
+    document.getElementById("qrcode").innerHTML = "";
+
+    new QRCode(document.getElementById("qrcode"), {
+        text: inputText,
+        width: 256,
+        height: 256,
+        colorDark: "#000000",
+        colorLight: "#ffffff"
+    });
+
+    document.getElementById("download-btn").style.display = "inline-block";
+}
+
 function generateWiFiQR() {
     const ssid = document.getElementById("ssid").value;
     const password = document.getElementById("password").value;
-    const wifiString = `WIFI:T:WPA;S:${ ssid };P:${ password };H:false;;`;
+    const security = document.getElementById("security").value;
+    const hidden = document.getElementById("hidden").checked;
+    const wifiString = `WIFI:T:${security};S:${ ssid };P:${ password };H:${hidden};;`;
     
-    document.getElementById("qr-canvas").innerHTML = "";
+    document.getElementById("qrcode").innerHTML = "";
     
-    let wifiQrcode = new QRCode(document.getElementById("qr-canvas"), {
+    new QRCode(document.getElementById("qrcode"), {
         text: wifiString,
         width: 256,
         height: 256,
         colorDark: "#000000",
         colorLight: "#ffffff"
     });
+    
+    document.getElementById("download-btn").style.display = "inline-block";
 }
 
-function generateQrCode() {
-    const inputText = document.getElementById("qr-input").value;
+function generateVCard() {
+    const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+    const org = document.getElementById("org").value;
+    const jobTitle = document.getElementById("job-title").value;
+    const address = document.getElementById("address").value;
+    const note = document.getElementById("note").value;
     
-    if (!inputText) {
-        !alert("Please enter a URL!");
-        return;
-    }
+    const vCard = `BEGIN:VCARD
+VERSION:3.0
+FN:${name}
+TEL:${phone}
+EMAIL:${email}
+ORG:${org}
+TITLE:${jobTitle}
+ADR:${address}
+NOTE:${note}
+END:VCARD`;
     
     document.getElementById("qrcode").innerHTML = "";
     
     new QRCode(document.getElementById("qrcode"), {
-        text: inputText,
+        text: vCard,
+        width: 256,
+        height: 256,
+        colorDark: "#000000",
+        colorLight: "#ffffff"
+    });
+
+    document.getElementById("download-btn").style.display = "inline-block";
+}
+
+function generateEmailQR() {
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const body = document.getElementById("msgBody").value;
+    
+    const emailString = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    
+    document.getElementById("qrcode").innerHTML = "";
+    
+    new QRCode(document.getElementById("qrcode"), {
+        text: emailString,
+        width: 256,
+        height: 256,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+    });
+    document.getElementById("download-btn").style.display = "inline-block";
+}
+
+function generateSmsQR() {
+    const phone = document.getElementById("phone").value;
+    const message = document.getElementById("message").value;
+    
+    const smsString = `sms:${phone}?body=${encodeURIComponent(message)}`;
+    
+    document.getElementById("qrcode").innerHTML = "";
+    
+    new QRCode(document.getElementById("qrcode"), {
+        text: smsString,
         width: 256,
         height: 256,
         colorDark: "#000000",
