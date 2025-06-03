c07ddbd810cedbe4b54c6af5aca0a62016d2bbe5
Westman728 <r.westman93@gmail.com>
2025-06-03 09:28:20 +0200

    restructure

--- QRTools/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace QRTools.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- QRTools/wwwroot/js/functions.js
+++
@@ -1,8 +0,0 @@
-function toggleInfo() {
-    const infoDiv = document.getElementById("info");
-    if (infoDiv.style.display === "none") {
-        infoDiv.style.display = "flex";
-    } else {
-        infoDiv.style.display = "none";
-    }
-}
